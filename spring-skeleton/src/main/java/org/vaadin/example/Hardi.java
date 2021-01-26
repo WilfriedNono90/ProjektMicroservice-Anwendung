9e4032e44629b53282d13eda5576159dd959390a
WilfriedNono90 <wilfried.nono90@gmail.com>
2021-01-26 21:05:06 +0100

    testhardi

--- spring-skeleton/src/main/java/org/vaadin/example/Hardi.java
+++ spring-skeleton/src/main/java/org/vaadin/example/Hardi.java
@@ -2,14 +2,19 @@ package org.vaadin.example;
 
 
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-@Route(value = "hardi",layout = MainLayout.class)
+@Route(value = "idplayer",layout = MainLayout.class)
 public class Hardi extends VerticalLayout{
 	
+	Grid<Player> grid =new Grid<>(Player.class);
+	TextField filtertext = new TextField();
 	public Hardi() {
+		
 		add(new H1("Microservice"));
 	}
 
